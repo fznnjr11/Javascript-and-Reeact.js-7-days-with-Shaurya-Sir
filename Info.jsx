049afc914268bc80d8439fdbe3115236736647f7
fznnjr11 <86476340+fznnjr11@users.noreply.github.com>
2021-06-27 02:40:37 +0530

    Add files via upload

---
+++ Info.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Info() {
+  return (
+    <div className="note">
+      <h1>AI and ML</h1>
+      <p>Artificial Intelligence and Machine Learning Bootcamp</p>
+    </div>
+  );
+}
+export default Info;
